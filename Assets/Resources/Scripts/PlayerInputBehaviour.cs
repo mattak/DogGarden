9e39c1be25d0c6564cf228da90407c713595092e
Takuma Maruyama <maruyama@mfac.jp>
2015-07-25 17:57:45 +0900

    none

---
+++ Assets/Resources/Scripts/PlayerInputBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx.Triggers;
+
+public class PlayerInputBehaviour : MonoBehaviour {
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+}
---
+++ Assets/Resources/Scripts/PlayerInputManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx.Triggers;
+
+public class PlayerInputManager : SingletonMonoBehaviourFast<PlayerInputManager> {
+	private GameObject selectedObject = null;
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
