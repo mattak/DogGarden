2307b8dc46c4907add3a782389d9c47e8a1367c4
Takuma Maruyama <maruyama@mfac.jp>
2015-07-25 16:49:58 +0900

    commit

---
+++ Assets/Resources/Scripts/CameraBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviour : MonoBehaviour {
+	public Camera camera;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void Awake() {
+		//camera.orthographicSize = Screen.width;
+	}
+}
