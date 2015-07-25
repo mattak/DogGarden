cac00f121da1babc1081e96d9bc2428293c0219b
Takuma Maruyama <maruyama@mfac.jp>
2015-07-26 02:50:00 +0900

    Create Start / Select Menu

---
+++ Assets/Resources/Scripts/StageSelectMenu/SelectButtonBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using System.Collections;
+
+public class SelectButtonBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Button>().OnClickAsObservable().Subscribe(_ => {
+			Application.LoadLevel ("Main");
+		});
+	}
+}
---
+++ Assets/Resources/Scripts/Top/StartButtonBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UniRx;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartButtonBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Button> ().OnClickAsObservable().Subscribe(_ => {
+			Application.LoadLevel ("StageSelectMenu");
+		});
+	}
+}
