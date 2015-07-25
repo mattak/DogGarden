ab26abcab5ffafebfc2c2481ea2a13e8c0cba30b
mattak <mattak.me@gmail.com>
2015-07-25 18:59:34 +0900

    scene background edited

--- Assets/Resources/Scripts/PlayerInputManager.cs
+++ Assets/Resources/Scripts/PlayerInputManager.cs
@@ -4,13 +4,4 @@ using UniRx.Triggers;
 
 public class PlayerInputManager : SingletonMonoBehaviourFast<PlayerInputManager> {
 	private GameObject selectedObject = null;
-	
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
