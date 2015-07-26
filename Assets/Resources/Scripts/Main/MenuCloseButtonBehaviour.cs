ce550f35bd45b11b6a6718d38580e13d1a6622ee
Takuma Maruyama <maruyama@mfac.jp>
2015-07-26 17:48:51 +0900

    menu close button behaviour

---
+++ Assets/Resources/Scripts/Main/MenuCloseButtonBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using System.Collections;
+
+public class MenuCloseButtonBehaviour : MonoBehaviour {
+	public GameObject hideLayer;
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Button> ().OnClickAsObservable().Subscribe(_ => {
+			hideLayer.SetActive (false);
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
