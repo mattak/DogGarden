905c5d4ad9a574c33a1820a16c68cbdf22566962
Takuma Maruyama <maruyama@mfac.jp>
2015-07-26 17:52:01 +0900

    add menu button

---
+++ Assets/Resources/Scripts/Main/MenuOpenButtonBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using System.Collections;
+
+public class MenuOpenButtonBehaviour : MonoBehaviour {
+	public GameObject menuLayer;
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Button>().OnClickAsObservable().Subscribe (_ => {
+			menuLayer.SetActive (true);
+		});
+	}
+}
